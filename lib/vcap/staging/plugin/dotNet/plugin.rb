51d6cc8bd63de5669139d7749da2e9c131af5c1d
mihaibuzgau <mihai.buzgau@uhurusoftware.com>
2012-07-20 20:26:06 +0300

    dotNet update
    Change-Id: I23f590f95e9fd3987d54604604c59ec81cd4ef4c

--- lib/vcap/staging/plugin/dotNet/plugin.rb
+++ lib/vcap/staging/plugin/dotNet/plugin.rb
@@ -1,6 +1,8 @@
 # Copyright (c) 2009-2011 VMware, Inc.
 # Copyright (c) 2011 Uhuru Software, Inc., All Rights Reserved
 class DotNetPlugin < StagingPlugin
+  require "json"
+
   def framework
     'dotNet'
   end
@@ -9,29 +11,32 @@ class DotNetPlugin < StagingPlugin
     Dir.chdir(destination_directory) do
       create_app_directories
       copy_source_files
-      copy_dea_plugin_assembly
       create_startup_script
     end
   end
 
   def generate_startup_script(env_vars = {})
-    after_env_before_script = block_given? ? yield : "\n"
-    template = <<-SCRIPT
-    Uhuru.CloudFoundry.DEA.Plugins.dll
-    Uhuru.CloudFoundry.DEA.Plugins.IISPlugin
-    SCRIPT
-    ERB.new(template).result(binding).lines.reject {|l| l =~ /^\s*$/}.join
+    plugin_staging_info = Hash.new
+    plugin_staging_info[:assembly] = "Uhuru.CloudFoundry.DEA.Plugins.dll"
+    plugin_staging_info[:class_name] = "Uhuru.CloudFoundry.DEA.Plugins.IISPlugin"
+    plugin_staging_info[:logs] = Hash.new
+    plugin_staging_info[:logs][:app_error] = "logs/stderr.log"
+    plugin_staging_info[:logs][:dea_error] = "logs/err.log"
+    plugin_staging_info[:logs][:startup] = "logs/startup.log"
+    plugin_staging_info[:logs][:app] = "logs/stdout.log"
+
+    plugin_staging_info[:auto_wire_templates] = Hash.new
+    plugin_staging_info[:auto_wire_templates]["mssql-2008"] = "Data Source={host},{port};Initial Catalog={name};User Id={user};Password={password};MultipleActiveResultSets=true"
+    plugin_staging_info[:auto_wire_templates]["mysql-5.1"] = "server={host};port={port};Database={name};Uid={user};Pwd={password};"
+
+    plugin_staging_info.to_json
   end
 
   private
 
-  def copy_dea_plugin_assembly
-
-  end
-
   def startup_script
     vars = environment_hash
-    generate_startup_script(vars)
+    generate_startup_script(vars)  
   end
 
 end
